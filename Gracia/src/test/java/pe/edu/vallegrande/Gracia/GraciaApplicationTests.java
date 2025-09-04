c226ee8091530cc242b4ab5bffbda9b9b98eee3a
GraciaGarcia <gracia.garcia@vallegrande.edu.pe>
2025-09-04 00:42:59 -0500

    realizados

---
+++ Gracia/src/test/java/pe/edu/vallegrande/Gracia/GraciaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.vallegrande.Gracia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
